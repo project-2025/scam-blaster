edc6b8ae4eecef99d6b554bbc4f8de5be6d7a7fb
Coodos <info@coodos.co>
2021-11-06 16:58:24 +0530

    changed help trigger

--- src/constants/triggers.ts
+++ src/constants/triggers.ts
@@ -1,5 +1,5 @@
 export const BOT_TRIGGER = "!blaster";
 export const ADD_WORD_TRIGGER = `${BOT_TRIGGER}-add`;
 export const REMOVE_WORD_TRIGGER = `${BOT_TRIGGER}-remove`;
-export const HELP_TRIGGER = `${BOT_TRIGGER}-help`;
 export const VIEW_WORDS = `${BOT_TRIGGER}-words`;
+export const HELP_TRIGGER = `!help`;
