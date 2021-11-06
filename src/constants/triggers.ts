ae971614f9151d054117be6c677f3eb6a1c7baf0
Coodos <info@coodos.co>
2021-11-06 16:56:33 +0530

    changed trigger

--- src/constants/triggers.ts
+++ src/constants/triggers.ts
@@ -1,4 +1,4 @@
-export const BOT_TRIGGER = ".blaster";
+export const BOT_TRIGGER = "!blaster";
 export const ADD_WORD_TRIGGER = `${BOT_TRIGGER}-add`;
 export const REMOVE_WORD_TRIGGER = `${BOT_TRIGGER}-remove`;
 export const HELP_TRIGGER = `${BOT_TRIGGER}-help`;
